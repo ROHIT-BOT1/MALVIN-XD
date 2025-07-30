a7bb6c4cc22844b50a8fe1759c2fd6c2d80af8a2
ROHIT-BOT1 <dasrakish77@gmail.com>
2025-07-31 02:27:28 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: "malvin~MvV0UAjZ#oo3Ihhf8-ArJPWEDfvwwlcDi59nz0o7ytAhpSOuR8Wg",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
